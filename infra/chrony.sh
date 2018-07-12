3f4d83adf78aa6e075f5488ffe51aeab6eeb0ed8
Paulo Ditarso <paulo.ditarso@gmail.com>
2018-07-12 22:50:32 +0000

    chrony script

---
+++ infra/chrony.sh
@@ -0,0 +1,8 @@
+sudo apt -qy install chrony >> apt-chrony.log 2>> apt-chrony-error.log
+
+sudo sh -c "echo '' >> /etc/chrony/chrony.conf"
+sudo sh -c "echo '# NECOS' >> /etc/chrony/chrony.conf"
+sudo sh -c "echo 'allow 10.0.0.0/24' >> /etc/chrony/chrony.conf"
+
+sudo service chrony restart
+if [ $? -ne 0 ]; then echo "NECOS: error"; fi
