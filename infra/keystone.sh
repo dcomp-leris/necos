0109730c9a96e7ef6d4d77bd192c9405f09ac282
Paulo Ditarso <paulo.ditarso@gmail.com>
2018-07-17 00:45:58 +0000

    updating files

--- infra/keystone.sh
+++ infra/keystone.sh
@@ -2,6 +2,8 @@ sudo mysql -uroot -psecret <<_EOF_
 CREATE DATABASE keystone;
 GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' IDENTIFIED BY 'secret';
 GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' IDENTIFIED BY 'secret';
+GRANT ALL PRIVILEGES ON keystone.* TO 'vagrant'@'localhost' IDENTIFIED BY 'secret';
+GRANT ALL PRIVILEGES ON keystone.* TO 'vagrant'@'%' IDENTIFIED BY 'secret';
 FLUSH PRIVILEGES;
 _EOF_
 
@@ -15,7 +17,7 @@ sudo sed -i 's/sqlite:\/\/\/\/var\/lib\/keystone\/keystone.db/mysql+pymysql:\/\/
 linhakeystone=`sudo awk '{if ($0 == "[token]") {print NR;}}' /etc/keystone/keystone.conf`
 sudo sed -i "$[linhakeystone+1] i\provider = fernet" /etc/keystone/keystone.conf
 
-sudo su -s /bin/bash keystone -c "keystone-manage db_sync" 
+sudo su -s /bin/sh -c "keystone-manage db_sync" keystone 
 sudo keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone
 sudo keystone-manage credential_setup --keystone-user keystone --keystone-group keystone
 
@@ -25,4 +27,6 @@ sudo sh -c "echo 'ServerName controller' >> /etc/apache2/apache2.conf"
 sudo service apache2 restart
 if [ $? -ne 0 ]; then echo "NECOS: error"; fi
 
-export OS_USERNAME=admin; export OS_PASSWORD=secret; export OS_PROJECT_NAME=admin; export OS_USER_DOMAIN_NAME=Default; export OS_PROJECT_DOMAIN_NAME=Default; export OS_AUTH_URL=http://controller:5000/v3; export OS_IDENTITY_API_VERSION=3
+echo "" >> /home/vagrant/.bashrc
+echo "# NECOS" >> /home/vagrant/.bashrc
+echo "export OS_USERNAME=admin; export OS_PASSWORD=secret; export OS_PROJECT_NAME=admin; export OS_USER_DOMAIN_NAME=Default; export OS_PROJECT_DOMAIN_NAME=Default; export OS_AUTH_URL=http://controller:5000/v3; export OS_IDENTITY_API_VERSION=3" >> /home/vagrant/.bashrc
