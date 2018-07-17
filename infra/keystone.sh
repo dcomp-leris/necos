bc036a8fd076726764a53f7ec761b0be7e39b8ef
Paulo Ditarso <paulo.ditarso@gmail.com>
2018-07-17 00:31:53 +0000

    updating files

--- infra/keystone.sh
+++ infra/keystone.sh
@@ -25,4 +25,4 @@ sudo sh -c "echo 'ServerName controller' >> /etc/apache2/apache2.conf"
 sudo service apache2 restart
 if [ $? -ne 0 ]; then echo "NECOS: error"; fi
 
-#export OS_USERNAME=admin; export OS_PASSWORD=secret; export OS_PROJECT_NAME=admin; export OS_USER_DOMAIN_NAME=Default; export OS_PROJECT_DOMAIN_NAME=Default; export OS_AUTH_URL=http://controller:5000/v3; export OS_IDENTITY_API_VERSION=3
+export OS_USERNAME=admin; export OS_PASSWORD=secret; export OS_PROJECT_NAME=admin; export OS_USER_DOMAIN_NAME=Default; export OS_PROJECT_DOMAIN_NAME=Default; export OS_AUTH_URL=http://controller:5000/v3; export OS_IDENTITY_API_VERSION=3
