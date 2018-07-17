18bcc2812c4cd21288d01281535d754f56480123
Paulo Ditarso <paulo.ditarso@gmail.com>
2018-07-17 00:04:09 +0000

    updating files

--- infra/mariadb.sh
+++ infra/mariadb.sh
@@ -3,21 +3,19 @@ sudo apt -qy install mariadb-server python-pymysql >> apt-mariadb.log 2>> apt-ma
 sudo sed -i 's/127.0.0.1/10.0.0.11/' /etc/mysql/mariadb.conf.d/50-server.cnf
 sudo sed -i 's/^#max_connections/max_connections/' /etc/mysql/mariadb.conf.d/50-server.cnf
 sudo sed -i 's/= 100$/= 4096/' /etc/mysql/mariadb.conf.d/50-server.cnf
-linha=`awk '{if ($0 == "[mysqld]") {print NR;}}' /etc/mysql/mariadb.conf.d/50-server.cnf`
-sudo sed -i "$[linha+1] i\innodb_file_per_table = on" /etc/mysql/mariadb.conf.d/50-server.cnf
+#linha=`awk '{if ($0 == "[mysqld]") {print NR;}}' /etc/mysql/mariadb.conf.d/50-server.cnf`
+#sudo sed -i "$[linha+1] i\innodb_file_per_table = on" /etc/mysql/mariadb.conf.d/50-server.cnf
 sudo service mysql restart
 if [ $? -ne 0 ]; then echo "NECOS: error"; fi
 
-#sudo mysql --user=root <<_EOF_
-#UPDATE mysql.user SET Password=PASSWORD('secret') WHERE User='root';
-#DELETE FROM mysql.user WHERE User='';
-#DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-#DROP DATABASE IF EXISTS test;
-#DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
-#FLUSH PRIVILEGES;
-#_EOF_
-
 sudo mysql --user=root <<_EOF_
+DROP USER 'root'@'localhost';
+CREATE USER 'root'@'%' IDENTIFIED BY '';
+UPDATE mysql.user SET Password=PASSWORD('secret') WHERE User='root';
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY 'secret';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'secret';
 FLUSH PRIVILEGES;
