11394c1a1530bb5e85623b195ec8861e4df313bc
Paulo Ditarso <paulo.ditarso@gmail.com>
2018-07-17 13:15:28 -0300

    updating locale.sh

--- infra/locale.sh
+++ infra/locale.sh
@@ -1,2 +1,8 @@
-sudo touch /var/lib/cloud/instance/locale-check.skip
+#sudo touch /var/lib/cloud/instance/locale-check.skip
+#if [ $? -ne 0 ]; then echo "NECOS: error"; fi
+
+locale-gen en_US en_US.UTF-8 pt_BR.UTF-8 >> locale-gen.log 2>> locale-gen-error.log
+if [ $? -ne 0 ]; then echo "NECOS: error"; fi
+
+sudo dpkg-reconfigure locales --default-priority >> dpkg-reconfigure.log 2>> dpkg-reconfigure-error.log
 if [ $? -ne 0 ]; then echo "NECOS: error"; fi
