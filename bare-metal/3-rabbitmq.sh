07c2cc9878fc38a8d4060b9a0d3012561b828e45
Nadaleti <32551299+Nadaleti@users.noreply.github.com>
2018-08-27 12:07:01 -0300

    Delete 3-rabbitmq.sh

--- bare-metal/3-rabbitmq.sh
+++
@@ -1,6 +0,0 @@
-sudo apt -qy install rabbitmq-server >> apt-rabbitmq.log 2>> apt-rabbitmq-error.log
-
-sudo rabbitmqctl add_user openstack devstack >> rabbitmq.log
-if [ $? -ne 0 ]; then echo "NECOS: error"; fi
-sudo rabbitmqctl set_permissions openstack ".*" ".*" ".*" >> rabbitmq.log
-if [ $? -ne 0 ]; then echo "NECOS: error"; fi
