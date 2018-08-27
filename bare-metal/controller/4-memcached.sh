eeb5a6521a86400ddec3c6517ab7bf1a17fe9c64
Nadaleti <mat.nadaleti@hotmail.com>
2018-08-27 12:01:16 -0300

    Separating scripts to be executing on controller and on compute

---
+++ bare-metal/controller/4-memcached.sh
@@ -0,0 +1,6 @@
+sudo apt -qy install memcached python-memcache >> apt-memcached.log 2>> apt-memcached-error.log
+
+sudo sed -i 's/127.0.0.1/10.0.0.17/' /etc/memcached.conf
+if [ $? -ne 0 ]; then echo "NECOS: error"; fi
+sudo service memcached restart
+if [ $? -ne 0 ]; then echo "NECOS: error"; fi
