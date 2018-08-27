f5b27c51e3b5fe28bb237d8c3836943b56a3275d
Nadaleti <32551299+Nadaleti@users.noreply.github.com>
2018-08-27 12:05:26 -0300

    Delete 0-apt.sh

--- bare-metal/0-apt.sh
+++
@@ -1,3 +0,0 @@
-sudo apt -qy update >> apt-update.log 2>> apt-update-error.log && sudo apt -qy upgrade >> apt-upgrade.log 2>> apt-upgrade-error.log
-
-sudo apt -qy install python-openstackclient >> apt-openstackclient.log 2>> apt-openstackclient-error.log
