50b82c6e3850b5fadcf79563e69f2f022a6a92d5
Andre Beltrami <debeltrami@gmail.com>
2018-09-19 10:47:20 -0300

    Fixing .log

--- vagrant/gnocchi.sh
+++ vagrant/gnocchi.sh
@@ -1,13 +1,13 @@
 
 
-openstack user create --domain default --password secret ceilometer >> /home/vagrant/logs/ceilometer.log 2>> /home/vagrant/logs/ceilometer-error.log
-openstack role add --project service --user ceilometer admin >> /home/vagrant/logs/ceilometer.log 2>> /home/vagrant/logs/ceilometer-error.log
-openstack user create --domain default --password secret gnocchi >> /home/vagrant/logs/ceilometer.log 2>> /home/vagrant/logs/ceilometer-error.log
-openstack service create --name gnocchi --description "Metric Service" metric >> /home/vagrant/logs/ceilometer.log 2>> /home/vagrant/logs/ceilometer-error.log
-openstack role add --project service --user gnocchi admin >> /home/vagrant/logs/ceilometer.log 2>> /home/vagrant/logs/ceilometer-error.log
-openstack endpoint create --region RegionOne metric public http://controller:8041 >> /home/vagrant/logs/ceilometer.log 2>> /home/vagrant/logs/ceilometer-error.log
-openstack endpoint create --region RegionOne metric internal http://controller:8041 >> /home/vagrant/logs/ceilometer.log 2>> /home/vagrant/logs/ceilometer-error.log
-openstack endpoint create --region RegionOne metric admin http://controller:8041 >> /home/vagrant/logs/ceilometer.log 2>> /home/vagrant/logs/ceilometer-error.log
+openstack user create --domain default --password secret ceilometer >> ceilometer.log 2>> ceilometer-error.log
+openstack role add --project service --user ceilometer admin >> ceilometer.log 2>> ceilometer-error.log
+openstack user create --domain default --password secret gnocchi >> ceilometer.log 2>> ceilometer-error.log
+openstack service create --name gnocchi --description "Metric Service" metric >> ceilometer.log 2>> ceilometer-error.log
+openstack role add --project service --user gnocchi admin >> ceilometer.log 2>> ceilometer-error.log
+openstack endpoint create --region RegionOne metric public http://controller:8041 >> ceilometer.log 2>> ceilometer-error.log
+openstack endpoint create --region RegionOne metric internal http://controller:8041 >> ceilometer.log 2>> ceilometer-error.log
+openstack endpoint create --region RegionOne metric admin http://controller:8041 >> ceilometer.log 2>> ceilometer-error.log
 
 sudo apt -qy install redis
 
