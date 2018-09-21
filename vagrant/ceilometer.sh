02ad17e9870c40fb6a11488ef0358ba49f5fc93d
iamfurukawa <32659391+iamfurukawa@users.noreply.github.com>
2018-09-20 23:29:57 -0300

    Update ceilometer.sh

--- vagrant/ceilometer.sh
+++ vagrant/ceilometer.sh
@@ -28,8 +28,7 @@ sudo sed -i "$[linhaDispatcherGnocchi+2] i\archive_policy = high" /etc/ceilomete
 sudo cp /usr/lib/python2.7/dist-packages/ceilometer/pipeline/data/*.yaml /etc/ceilometer/
 
 echo "gnocchi update"
-sudo gnocchi-upgrade --config-file /etc/gnocchi/gnocchi.conf >> gnocchiUpgrade.log 2>> gnocchiUpgrade-error.log
-sudo ln -s /etc/init.d/gnocchi-api /etc/rc3.d/S99gnocchi-metricd
+#sudo gnocchi-upgrade --config-file /etc/gnocchi/gnocchi.conf >> gnocchiUpgrade.log 2>> gnocchiUpgrade-error.log
 echo "ceilometer update"
 sudo ceilometer-upgrade --config-file /etc/ceilometer/ceilometer.conf >> ceilometerUpgrade.log 2>> ceilometerUpgrade-error.log
 echo "restart 1"
