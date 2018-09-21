6231b1fe045a65a30f79781e5453f0161970542c
Nadaleti <32551299+Nadaleti@users.noreply.github.com>
2018-09-21 09:38:11 -0300

    Update gnocchi.sh
    Updating gnocchi.sh to start gnocchi-api and gnocchi-metricd when finish the script

--- vagrant/gnocchi.sh
+++ vagrant/gnocchi.sh
@@ -87,4 +87,6 @@ gnocchi-metricd --config-file /etc/gnocchi/gnocchi.conf >> gnocchiMetricd.log 2>
 _EOF_
 
 sudo ln -s /etc/init.d/gnocchi-api /etc/rc3.d/S99gnocchi-metricd
+gnocchi-api &
+gnocchi-metricd &
 
