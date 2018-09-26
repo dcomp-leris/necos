b8eaf1de84e4018e6007d08ed4a624ff4384b75f
Andre Beltrami <debeltrami@gmail.com>
2018-09-26 16:34:15 -0300

    Issue rabbitmq

--- vagrant/rabbitmq.sh
+++ vagrant/rabbitmq.sh
@@ -1,6 +1,6 @@
 sudo apt -qy install rabbitmq-server >> apt-rabbitmq.log 2>> apt-rabbitmq-error.log
 
-sudo rabbitmqctl add_user openstack $2 >> rabbitmq.log
+sudo rabbitmqctl add_user openstack $1 >> rabbitmq.log
 if [ $? -ne 0 ]; then echo "NECOS: error"; fi
 sudo rabbitmqctl set_permissions openstack ".*" ".*" ".*" >> rabbitmq.log
 if [ $? -ne 0 ]; then echo "NECOS: error"; fi
