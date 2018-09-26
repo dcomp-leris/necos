20affd8ca29cc3c0d57eaa4bdf3b8e9b81822752
Andre Beltrami <debeltrami@gmail.com>
2018-09-26 14:30:13 -0300

    mariadb issue1

--- vagrant/mariadb.sh
+++ vagrant/mariadb.sh
@@ -1,7 +1,7 @@
 sudo apt -qy install mariadb-server >> apt-mariadb.log 2>> apt-mariadb-error.log
 sudo apt -qy install python-pymysql >> apt-pymysql.log 2>> apt-pymysql-error.log
 
-sudo sed -i 's/127.0.0.1/$1/' /etc/mysql/mariadb.conf.d/50-server.cnf
+sudo sed -i "s/127.0.0.1/$1/" /etc/mysql/mariadb.conf.d/50-server.cnf
 sudo sed -i 's/^#max_connections/max_connections/' /etc/mysql/mariadb.conf.d/50-server.cnf
 sudo sed -i 's/= 100$/= 4096/' /etc/mysql/mariadb.conf.d/50-server.cnf
 sudo sed -i 's/utf8mb4/utf8/' /etc/mysql/mariadb.conf.d/50-server.cnf
@@ -11,9 +11,9 @@ sudo service mysql restart
 if [ $? -ne 0 ]; then echo "NECOS: error"; fi
 
 sudo mysql --user=root <<_EOF_
-UPDATE mysql.user SET plugin='', Password=PASSWORD('$2') WHERE User='root';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '$2';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$2';
+UPDATE mysql.user SET plugin='', Password=PASSWORD("$2") WHERE User='root';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY "$2";
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY "$2";
 DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DROP DATABASE IF EXISTS test;
