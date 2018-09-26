b1c904faac50507efa82090b8e06754543b333a5
Andre Beltrami <debeltrami@gmail.com>
2018-09-26 13:42:52 -0300

    Vagrant File test 1

--- vagrant/devstack-AiO.sh
+++
@@ -1,8 +0,0 @@
-useradd -s /bin/bash -d /opt/stack -m stack
-echo "stack ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/stack
-su - stack
-git clone https://git.openstack.org/openstack-dev/devstack
-cd devstack
-echo -e "[[local|localrc]]\nADMIN_PASSWORD=$2\nDATABASE_PASSWORD=$2\nRABBIT_PASSWORD=$2\nSERVICE_PASSWORD=$2" >> local.conf
-echo "HOST_IP=`hostname -I | cut -d " " -f 2`" >> local.conf
-./stack.sh
