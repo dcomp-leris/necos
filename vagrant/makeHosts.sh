471833b2c200a12ecf564cffc4257565ecaca5a6
Paulo Ditarso <paulo.maciel@ifpb.edu.br>
2018-10-02 14:25:31 -0300

    updating makeHosts.sh

--- vagrant/makeHosts.sh
+++ vagrant/makeHosts.sh
@@ -1,7 +1,6 @@
 ipController=$1
 ipCompute1=$2
 
-
 sudo tee /etc/hosts 1>/dev/null <<_EOF_
 127.0.0.1	localhost
 # controller
@@ -9,3 +8,5 @@ $ipController       controller
 # compute1 and compute2
 $ipCompute1       compute1
 _EOF_
+
+sudo sed -i "s/ - update_etc_hosts/# - update_etc_hosts/" /etc/cloud/cloud.cfg
