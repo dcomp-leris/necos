e0564c08e361ac14e286bf181691b17d72cfedb5
iamfurukawa <vinicius_furuka@hotmail.com.br>
2018-11-26 12:13:51 -0200

    a

--- stress/stress_scheduler.py
+++ stress/stress_scheduler.py
@@ -5,15 +5,15 @@ def job():
 	print("I'm working...")
 	if 21 < hour and hour <= 6:
 		print("Slow...")
-		os.system('stress -c 2 -i 1 -m 1 --vm-bytes 128M -t 3600s & \iperf3 -c speedtest.wtnet.de -p 5201 -t 3600 -b 10M')
+		os.system('stress -c 2 -i 1 -m 1 --vm-bytes 128M -t 57m & \iperf3 -c speedtest.wtnet.de -p 5201 -t 3420 -b 10M')
 
 	if (6 < hour and hour <= 9) or (18 < hour and hour <= 21):
 		print("Medium...")
-		os.system('stress -c 2 -i 1 -m 1 --vm-bytes 512M -t 3600s & \iperf3 -c speedtest.wtnet.de -p 5201 -t 3600 -b 100M')
+		os.system('stress -c 2 -i 1 -m 1 --vm-bytes 512M -t 57m & \iperf3 -c speedtest.wtnet.de -p 5201 -t 3420 -b 100M')
 
 	if 9 < hour and hour <= 18:
 		print("Hard...")
-		os.system('stress -c 2 -i 1 -m 1 --vm-bytes 1024M -t 3600s & \iperf3 -c speedtest.wtnet.de -p 5201 -t 3600 -b 1G')
+		os.system('stress -c 2 -i 1 -m 1 --vm-bytes 1024M -t 57m & \iperf3 -c speedtest.wtnet.de -p 5201 -t 3420 -b 1G')
 
 	print(time.localtime().tm_hour,':', time.localtime().tm_min)
 
