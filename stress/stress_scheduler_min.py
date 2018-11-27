b51c50e32a8640721f85882c11dc889b191d7d12
iamfurukawa <32659391+iamfurukawa@users.noreply.github.com>
2018-11-27 19:43:05 -0200

    version mini

---
+++ stress/stress_scheduler_min.py
@@ -0,0 +1,39 @@
+import schedule, subprocess, os, sys, time
+
+def job():
+	hour = time.localtime().tm_hour
+	print("I'm working...")
+	if hour % 3 == 0:
+		print("Slow...")
+		os.system('stress -c 2 -i 1 -m 1 --vm-bytes 128M -t 50m & \iperf3 -c speedtest.wtnet.de -p 5201 -t 3000 -b 10M')
+
+	if hour % 3 == 1:
+		print("Medium...")
+		os.system('stress -c 2 -i 1 -m 1 --vm-bytes 512M -t 50m & \iperf3 -c speedtest.wtnet.de -p 5201 -t 3000 -b 100M')
+
+	if hour % 3 == 2:
+		print("Hard...")
+		os.system('stress -c 2 -i 1 -m 1 --vm-bytes 1024M -t 50m & \iperf3 -c speedtest.wtnet.de -p 5201 -t 3000 -b 1G')
+
+	print(time.localtime().tm_hour,':', time.localtime().tm_min)
+
+schedule.every(60).minutes.do(job)
+
+while 1:
+	schedule.run_pending()
+	time.sleep(1)
+
+'''
+FROM ubuntu:latest
+RUN \
+  apt-get update && \
+  apt-get -y upgrade && \
+  apt-get -y install python3-pip && \
+  pip3 install schedule && \
+  apt-get -y install iperf3 && \
+  apt-get -y install stress && \
+  apt-get -y install git && \
+  git clone https://github.com/dcomp-leris/necos.git
+
+ENTRYPOINT python3 necos/stress/stress_scheduler_min.py
+'''
