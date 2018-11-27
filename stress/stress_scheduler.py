784442951bba981e683aaec033b4993f5d1cf444
iamfurukawa <32659391+iamfurukawa@users.noreply.github.com>
2018-11-27 19:32:02 -0200

    add apt pip3

--- stress/stress_scheduler.py
+++ stress/stress_scheduler.py
@@ -28,7 +28,7 @@ FROM ubuntu:latest
 RUN \
   apt-get update && \
   apt-get -y upgrade && \
-    && \
+  apt-get -y install python3-pip && \
   pip3 install schedule && \
   apt-get -y install iperf3 && \
   apt-get -y install stress && \
