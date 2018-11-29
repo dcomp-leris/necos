e247563552441df9f1ab8e934b85550d1f1db88a
andrebeltrami <andreluizbeltrami@hotmail.com>
2018-11-29 11:26:30 -0200

    Update stress_scheduler_min.py

--- stress/stress_scheduler_min.py
+++ stress/stress_scheduler_min.py
@@ -5,15 +5,15 @@ def job():
 	print("I'm working...")
 	if hour % 3 == 0:
 		print("Slow...")
-		os.system('stress -c 2 -i 1 -m 1 --vm-bytes 128M -t 60m & \iperf3 -c 200.136.191.117 -p 5000 -t 3600 -b 10M')
+		os.system('stress -c 2 -i 1 -m 1 --vm-bytes 128M -t 60m & \iperf -c 200.136.191.117 -p 5000 -t 3600 -b 10M')
 
 	if hour % 3 == 1:
 		print("Medium...")
-		os.system('stress -c 2 -i 1 -m 1 --vm-bytes 512M -t 60m & \iperf3 -c 200.136.191.117 -p 5000 -t 3600 -b 100M')
+		os.system('stress -c 2 -i 1 -m 1 --vm-bytes 512M -t 60m & \iperf -c 200.136.191.117 -p 5000 -t 3600 -b 100M')
 
 	if hour % 3 == 2:
 		print("Hard...")
-		os.system('stress -c 2 -i 1 -m 1 --vm-bytes 1024M -t 60m & \iperf3 -c 200.136.191.117 -p 5000 -t 3600 -b 1G')
+		os.system('stress -c 2 -i 1 -m 1 --vm-bytes 1024M -t 60m & \iperf -c 200.136.191.117 -p 5000 -t 3600 -b 1G')
 
 	print(time.localtime().tm_hour,':', time.localtime().tm_min)
 
@@ -31,6 +31,7 @@ RUN \
   apt-get -y install python3-pip && \
   pip3 install schedule && \
   apt-get -y install iperf3 && \
+  apt-get -y install iperf &&\
   apt-get -y install stress && \
   apt-get -y install git && \
   git clone https://github.com/dcomp-leris/necos.git
