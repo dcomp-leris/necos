bf490d1bb16e11ceb61514f1ccdee200c401ac99
andrebeltrami <andreluizbeltrami@hotmail.com>
2018-11-30 13:00:35 -0200

    Update stress_scheduler_min.py

--- stress/stress_scheduler_min.py
+++ stress/stress_scheduler_min.py
@@ -7,21 +7,35 @@ def job(profile):
 	if profile == 0:
 		print("Slow...")
 		os.system('stress-ng -c 1 -l 15% --io 1 --hdd-bytes 10m --vm 1 --vm-bytes 10% -t 10m & \iperf -c 200.136.191.117 -p 5000 -t 600 -b 10K')
-
 	if profile == 1:
 		print("Medium...")
-		os.system('stress-ng -c 1 -l 40% -d 1 --hdd-bytes 10m --vm 1 --vm-bytes 40% -t 10m & \iperf -c 200.136.191.117 -p 5000 -t 600 -b 1M')
-
+		os.system('stress-ng -c 1 -l 40% -d 1 --hdd-bytes 10m --vm 1 --vm-bytes 40% -t 10m & \iperf -c 200.136.191.117 -p 5000 -t 600 -b 5M')
 	if profile == 2:
 		print("Hard...")
 		os.system('stress-ng -c 1 -l 80% -d 1 --hdd-bytes 100m --vm 1 --vm-bytes 80% -t 10m & \iperf -c 200.136.191.117 -p 5000 -t 600 -b 50M')
 
+def jobCPU(profile):
+	if profile == 0:
+		print("Slow...")
+		os.system('stress-ng -c 1 -l 15% -t 5m')
+	if profile == 1:
+		print("Medium...")
+		os.system('stress-ng -c 1 -l 40% -t 5m')
+	if profile == 2:
+		print("Hard...")
+		os.system('stress-ng -c 1 -l 80% -t 5m')
+	
 	print(time.localtime().tm_hour,':', time.localtime().tm_min)
 
 i = 0
 while i < 3:
 	job(i)
 	i += 1
+i = 0
+while i < 3:
+	jobCPU(i)
+	i += 1
+	
 '''
 FROM ubuntu:latest
 RUN \
